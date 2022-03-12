6ef14c93c0fc84a6e2fad641ed31799091b4aba2
zRain <2296342883@qq.com>
2022-03-12 14:08:08 +0800

    ci: 🎡 add jest and parcel

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  preset: 'ts-jest',
+  testEnvironment: 'node'
+}
